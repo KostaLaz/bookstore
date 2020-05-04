2591f5594551cc83013384ead7fc03c0b340fc8f
kosta.lazarevski@inellipse.com <kosta.lazarevski@inellipse.com>
2020-05-05 00:21:00 +0200

    Service

--- src/main/java/com/bookstore/bookstore/service/BookstoreService.java
+++ src/main/java/com/bookstore/bookstore/service/BookstoreService.java
@@ -1,7 +1,18 @@
 package com.bookstore.bookstore.service;
 
+import com.bookstore.bookstore.domain.Book;
+
+import java.util.List;
+import java.util.Set;
+
 public interface BookstoreService {
 
+    Set<Book> getAllBooks();
 
+    List<Book> getBooksByAuthor(String author);
+
+    List<Book> getBooksByTitle(String title);
+
+    List<Book> getBooksByGenre(String genre);
 
 }
--- src/main/java/com/bookstore/bookstore/service/BookstoreServiceImpl.java
+++ src/main/java/com/bookstore/bookstore/service/BookstoreServiceImpl.java
@@ -1,4 +1,30 @@
 package com.bookstore.bookstore.service;
 
-public class BookstoreServiceImpl {
+import com.bookstore.bookstore.domain.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class BookstoreServiceImpl implements BookstoreService{
+    @Override
+    public Set<Book> getAllBooks() {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksByAuthor(String author) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksByTitle(String title) {
+        return null;
+    }
+
+    @Override
+    public List<Book> getBooksByGenre(String genre) {
+        return null;
+    }
 }
