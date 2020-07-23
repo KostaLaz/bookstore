d1a2c3d58b1a362bfe16924421990e5697c4df9b
kosta <kosta.lazarevski89@gmail.com>
2020-07-23 14:17:48 +0200

    Repository

--- src/main/java/com/bookstore/bookstore/repository/BookRepository.java
+++ src/main/java/com/bookstore/bookstore/repository/BookRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BookstoreRepository extends JpaRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findAll();
 
