34461b4162dd7d69040622e64083e64c7c7f3fd6
kosta <kosta.lazarevski89@gmail.com>
2020-07-29 16:35:53 +0200

    Order model added

--- src/main/java/com/bookstore/bookstore/domain/Order.java
+++ src/main/java/com/bookstore/bookstore/domain/Order.java
@@ -1,4 +1,34 @@
 package com.bookstore.bookstore.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+import java.util.Map;
+
+@Document(collection = "Order")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Order {
+
+    @Id
+    private Integer id;
+
+    private Map<Book, Integer> items;
+
+    private Date orderOn;
+
+    private double totalPrice;
+
+    private Client orderBy;
+
+    public Order(Map<Book, Integer> items, double totalPrice, Client orderBy) {
+        this.items = items;
+        this.totalPrice = totalPrice;
+        this.orderBy = orderBy;
+    }
 }
